652cf5df249ea67752ceb9af77417b50675652ab
Kwonelee <995546054@qq.com>
2025-12-19 14:03:53 +0800

    Update 00-prepare_base.sh

--- diy/00-prepare_base.sh
+++ diy/00-prepare_base.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 # Rockchip - rkbin & u-boot
-rm -rf package/boot/rkbin package/boot/uboot-rockchip package/boot/arm-trusted-firmware-rockchip
-if [ "$platform" = "rk3399" ]; then
-    git clone https://$github/sbwml/package_boot_uboot-rockchip package/boot/uboot-rockchip -b v2023.04
-    git clone https://$github/sbwml/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip -b 0419
-else
-    git clone https://$github/sbwml/package_boot_uboot-rockchip package/boot/uboot-rockchip
-    git clone https://$github/sbwml/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip
-fi
+#rm -rf package/boot/rkbin package/boot/uboot-rockchip package/boot/arm-trusted-firmware-rockchip
+#if [ "$platform" = "rk3399" ]; then
+    #git clone https://$github/sbwml/package_boot_uboot-rockchip package/boot/uboot-rockchip -b v2023.04
+    #git clone https://$github/sbwml/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip -b 0419
+#else
+    #git clone https://$github/sbwml/package_boot_uboot-rockchip package/boot/uboot-rockchip
+    #git clone https://$github/sbwml/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip
+#fi
 
 # patch source
 curl -s $mirror/openwrt/patch/generic-24.10/0001-tools-add-upx-tools.patch | patch -p1
